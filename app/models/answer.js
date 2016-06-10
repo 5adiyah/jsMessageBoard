0c9e9fa7355a48858b77d51ca085387affed808d
5adiyah <sadiali@pdx.edu>
2016-06-10 10:41:40 -0700

    add answer-detail component

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,11 +1,12 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-// import { belongsTo, hasMany } from 'ember-data/relationships';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   author: attr(),
   avatar: attr(),
   date: attr(),
   // attr('date', { defaultValue() { return new Date(); } }),
-  answer: attr(),
+  content: attr(),
+  question: belongsTo('question', {async: true})
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return Ember.RSVP.has({
+    return Ember.RSVP.hash({
       questions: this.store.findAll('question'),
       answers: this.store.findAll('answer')
     })
