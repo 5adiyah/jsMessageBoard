f767fe5ac533b0b3fc8a540ec1c320e052655459
5adiyah <sadiali@pdx.edu>
2016-06-10 09:37:07 -0700

    Add new question feature

--- app/components/question-new.js
+++ app/components/question-new.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow(){
+      this.set('addNewQuestion', true);
+    },
+
+    save1(){
+      var params = {
+        author: this.get('author'),
+        avatar: this.get('avatar'),
+        date: this.get('date'),
+        question: this.get('question'),
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('save2', params);
+    }
+  }
 });
