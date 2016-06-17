9c3025b206b90dec5769d3de5a3749cd5d677d45
5adiyah <sadiali@pdx.edu>
2016-06-17 10:05:16 -0700

    Committing

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,11 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // ratingScore: Ember.computed('model.questions', function(){
-  //   var score = this.get('question').get('answers').get('length');
-  //   console.log("score = " + score);
-  //   return score;
-  // })
   actions: {
     upVote(question){
       console.log("________________________");
@@ -16,10 +11,7 @@ export default Ember.Component.extend({
     },
 
     downVote(question){
-      console.log("________________________");
-      console.log("Question score question-tile: " + question.get('score'));
       var score = question.get('score') - 1;
-      console.log("score in question-tile: " + score);
       this.sendAction('upVote', question, score)
     },
 
