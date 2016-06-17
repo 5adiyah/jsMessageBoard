cac826bb30eda3334c8eabbd00a98cedb94dc38a
5adiyah <sadiali@pdx.edu>
2016-06-17 11:39:06 -0700

    Add computed property to animal-detail, displays num animals

--- app/components/answer-detail.js
+++ app/components/answer-detail.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  numAnswers: Ember.computed('model', function(){
+    var numAnswers = this.get('model').get('answers').get('length');
+    return numAnswers;
+  }),
+
   actions: {
     saveAnswer(params){
       this.sendAction('saveAnswer', params);
--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
   actions: {
     upVote(question){
       console.log("________________________");
